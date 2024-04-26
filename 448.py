96810b8ecb6f777c161f1254dd557362662a74ce
Bony Patel <bonyp96@gmail.com>
2024-04-26 13:02:38 -0400

    Working solution for 448.py using sets :)

--- 448.py
+++ 448.py
@@ -1,15 +1,26 @@
-    # Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+# Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range 
+# [1, n] that do not appear in nums.
 
-# COME BACK AND FIGURE OUT HOW TO DO IT WITH A HASHSET ITS NOT FUCKING POSSIBLE
+# SOLVED BY BACKTRACKING WITH SETS. TRY NOT TO BACK TRACK WITH BRUTE FORCE
+# Time complexity: O(n^2)
+# Space complexity: O(n2^n)
 class Solution:
     def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
         ans = []
+        ans_set = set(nums)
+        test = set()
+        
         for i in range(1, len(nums) + 1):
-            if i not in nums:
-                ans.append(i)
+            test.add(i)
+
+        test = test - ans_set
+
+        for i in range(len(test)):
+            ans.append(test.pop())
+
         return ans
     
 if __name__ == "__main__":
     my_solution = Solution()
-    test = [4,3,2,7,8,2,3,1]
+    test = [1,1]
     my_solution.findDisappearedNumbers(test)
